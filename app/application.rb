f4ea6bc52fd8efedd63583d5b928af7e8af9bbda
Anika Christopher <anika.mchristopher@gmail.com>
2020-01-20 05:10:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    time = DateTime.now.new_offset
+    time = DateTime.now
 
     if time <= 12 
       resp.write "Good Morning!"
